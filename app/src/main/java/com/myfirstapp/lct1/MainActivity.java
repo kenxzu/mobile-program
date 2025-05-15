9f9390b41baa7d9ee04be2c695ddb3a67033bbc0
KenFoster <kenmorintoh96@gmail.com>
2025-05-15 14:57:23 +0700

    fixed tiddy

--- app/src/main/java/com/myfirstapp/lct1/MainActivity.java
+++ app/src/main/java/com/myfirstapp/lct1/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -23,14 +24,14 @@ public class MainActivity extends AppCompatActivity {
     public void btnTouched(View v){
         String data = et1.getText().toString();
         th.setText(data);
-        try {
-            int angka = Integer.parseInt(data);
-            float hasil = (float) Math.PI * angka * angka;
-            String h = String.valueOf(hasil);
-            th.setText(h);
-        } catch (Exception ex){
-
-        }
+//        try {
+//            int angka = Integer.parseInt(data);
+//            float hasil = (float) Math.PI * angka * angka;
+//            String h = String.valueOf(hasil);
+//            th.setText(h);
+//        } catch (Exception ex){
+//
+//        }
 
     }
 
